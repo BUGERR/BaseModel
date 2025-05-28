90bb06625af4e8f2fb7087b42c3cde1f3a41d90b
BUGERR <45065853+BUGERR@users.noreply.github.com>
2025-05-28 18:30:29 +0800

    Delete Transformer_iwslt17/modules directory

--- Transformer_iwslt17/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from .transformer import Transformer, make_pad_mask, make_tgt_mask, make_causal_mask
-from .decoder import Decoder, DecoderLayer
-from .embedding import TransformerEmbedding
-from .encoder import Encoder, EncoderLayer
